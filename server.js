edc5eb4ae986df5ef9932907131a31ee35a86bbf
deborah <112716483+edebbyi@users.noreply.github.com>
2025-11-09 21:44:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+console.log('==> Starting server.js...');
+console.log('==> Node version:', process.version);
+console.log('==> Environment:', process.env.NODE_ENV);
+console.log('==> DATABASE_URL exists:', !!process.env.DATABASE_URL);
+console.log('==> About to require modules...');
+
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
